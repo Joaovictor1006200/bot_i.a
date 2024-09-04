ffe01ca28640b3d209929dd58547d748700166a9
Joaovictor1006200 <00001132744556SP@al.educacao.sp.gov.br>
2024-09-04 11:04:16 -0300

    primeiro commit

--- bot.py
+++ bot.py
@@ -11,6 +11,7 @@ class SimpleChatbot:
             'como você está?': ['Estou bem, obrigado!', 'Tudo certo, e você?'],
             'qual é o seu nome?': ['Eu sou um chatbot sem nome.', 'Você pode me chamar de Chatbot.'],
             'tchau': ['Tchau!', 'Até logo!', 'Adeus!'],
+            'explique o que é git pull': ['comando para puxar do repositório remoto para o local'],
             'passa a resposta da lição': ['qual é a lição?'],
             'lição de ia': ['qual semana e aula?'],
             's15- aula- 3': ['a resposta é criar perguntas para o chat bot interagir com você']
